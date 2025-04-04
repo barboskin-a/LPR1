0a583f9e0141384299fcf089a609cb93df2627a2
barboskin-a <lizik.rabotyga45@gmail.com>
2025-04-04 22:02:46 +0700

    Практическая работа №8 product-details

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,5 +1,19 @@
 let product = "Socks";
 
+Vue.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+    template: `
+        <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+    `
+})
+
 Vue.component('product', {
     props: {
         premium: {
