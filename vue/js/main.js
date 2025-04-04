b9cfb1cf70df48414f5413060230c1d1b9ab0f35
barboskin-a <lizik.rabotyga45@gmail.com>
2025-04-04 12:51:12 +0700

    практическая 4 add size

--- vue/js/main.js
+++ vue/js/main.js
@@ -17,6 +17,7 @@ let app = new Vue({
                 variantId: 2235,
                 variantColor: 'blue'
             }
-        ]
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 })
